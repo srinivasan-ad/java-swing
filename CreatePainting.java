44f49fd8af13b3edaafce4212ec16802ac28669f
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-05-07 22:03:46 +0530

    commiting after a small break :zap:

---
+++ CreatePainting.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+public class CreatePainting {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simple Drawing");
+        frame.setSize(400,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
